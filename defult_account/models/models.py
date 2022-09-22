b15ca4f8fc72495647316228d9e4548c9930469e
hnadi-gnan <hnadi.gnan@gmail.com>
2022-09-22 14:32:29 +0000

    last one

--- defult_account/models/models.py
+++ defult_account/models/models.py
@@ -21,16 +21,32 @@ from odoo import models, fields, api
 
 
 
-# class defult_account(models.Model):
-#     _name = 'defult_account.defult_account'
-#     _description = 'defult_account.defult_account'
+class defult_account(models.Model):
+    _name = 'defult_account.defult_account'
+    _description = 'defult_account.defult_account'
+    
+    def _get_default_currency_id(self):
+        return self.env.company.currency_id.id
+    
+    company_id = fields.Many2one('res.company', string='Company', index=True, default=lambda self: self.env.company)
+    currency_id = fields.Many2one("res.currency", string="Currency", required=True ,default = _get_default_currency_id)
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record in self:
-#             record.value2 = float(record.value) / 100
+    currency_exchange_journal_id = fields.Many2one(
+        comodel_name='account.journal',
+        related='company_id.currency_exchange_journal_id', readonly=False,
+        string="Currency Exchange Journal",
+        help='The accounting journal where automatic exchange differences will be registered')
+    
+    income_currency_exchange_account_id = fields.Many2one(
+        comodel_name="account.account",
+        related="company_id.income_currency_exchange_account_id",
+        string="Gain Account",
+        readonly=False)
+    
+    expense_currency_exchange_account_id = fields.Many2one(
+        comodel_name="account.account",
+        related="company_id.expense_currency_exchange_account_id",
+        string="Loss Account",
+        readonly=False)
+    
+    
